12016bb2a072912f188ed6c2628b1be1ede99a35
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-02 15:06:41 +0530

    Added Week 5 Assignment

---
+++ DOTNET/Week-5-Assignment/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static double CalculateNetSalary(double basicSalary)
+        {
+            try
+            {
+                if (basicSalary < 0)
+                {
+                    throw new Exception("Basic salary should be greater than zero!!!!!");
+                }
+                double hra = 0.20 * basicSalary;
+                double da = 0.10 * basicSalary;
+                double pf = 0.12 * basicSalary;
+                if (basicSalary < 15000)
+                {
+                    return basicSalary + hra + da;
+                }
+                return basicSalary + hra + da - pf;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+    }
+}
