1bf708d86f9666e82965c3ef202344a7cebc3bab
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-08 19:16:01 +0530

    Add files via upload

---
+++ DOTNET/Week-6-ASSMT/Requirement2/Vehicle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Requirement2
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+        }
+
+        public Vehicle(string _registrationNo, string _name, string _type, double _weight, Ticket _ticket)
+        {
+            this._registrationNo = _registrationNo;
+            this._name = _name;
+            this._type = _type;
+            this._weight = _weight;
+            this._ticket = _ticket;
+        }
+
+        private string _registrationNo;
+        private string _name;
+        private string _type;
+        private double _weight;
+        private Ticket _ticket;
+
+        public string RegistrationNo
+        {
+            get { return _registrationNo; }
+            set { _registrationNo = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        public double Weight
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public Ticket Ticket
+        {
+            get { return _ticket; }
+            set { _ticket = value; }
+        }
+
+        // method to create Vehicle from comma-separated input
+        public static Vehicle CreateVehicle(string detail)
+        {
+            string[] input = detail.Split(',');
+            Ticket ticket = new Ticket(input[4],DateTime.ParseExact(input[5], "dd-MM-yyyy HH:mm:ss", null),double.Parse(input[6]));
+            Vehicle vehicle = new Vehicle(input[0],input[1],input[2],double.Parse(input[3]),ticket);
+            return vehicle;
+        }
+    }
+}
---
+++ DOTNET/Week-6-ASSMT/Requirement4/VehicleBO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Requirement4
+{
+    internal class VehicleBO
+    {
+        // Finds vehicles matching the given type
+        public List<Vehicle> FindVehicle(List<Vehicle> vehicleList, string type)
+        {
+            List<Vehicle> vehiclesWithGivenType = new List<Vehicle>();
+            foreach (Vehicle vehicle in vehicleList)
+            {
+                if (vehicle.Type == type)
+                {
+                    vehiclesWithGivenType.Add(vehicle);
+                }
+            }
+            return vehiclesWithGivenType;
+        }
+
+        // Finds vehicles matching the given parked time
+        public List<Vehicle> FindVehicle(List<Vehicle> vehicleList, DateTime parkedTime)
+        {
+            List<Vehicle> vehiclesWithParkedTime = new List<Vehicle>();
+            foreach (Vehicle vehicle in vehicleList)
+            {
+                if (vehicle.Ticket.ParkedTime == parkedTime)
+                {
+                    vehiclesWithParkedTime.Add(vehicle);
+                }
+            }
+            return vehiclesWithParkedTime;
+        }
+    }
+}
