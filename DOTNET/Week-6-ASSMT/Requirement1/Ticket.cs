693836e860351f5b3cbfd6dd7cfee494f70e966c
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-08 19:13:58 +0530

    Add files via upload

---
+++ DOTNET/Week-6-ASSMT/Requirement1/Ticket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Requirement1
+{
+    public class Ticket
+    {
+        public Ticket()
+        {
+        }
+
+        public Ticket(string _ticketNo, DateTime _parkedTime, double _cost)
+        {
+            this._ticketNo = _ticketNo;
+            this._parkedTime = _parkedTime;
+            this._cost = _cost;
+        }
+
+        private string _ticketNo;
+
+        public string TicketNo
+        {
+            get { return _ticketNo; }
+            set { _ticketNo = value; }
+        }
+
+        private DateTime _parkedTime;
+
+        public DateTime ParkedTime
+        {
+            get { return _parkedTime; }
+            set { _parkedTime = value; }
+        }
+
+        private double _cost;
+
+        public double Cost
+        {
+            get { return _cost; }
+            set { _cost = value; }
+        }
+    }
+}
