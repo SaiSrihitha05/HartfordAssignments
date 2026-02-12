af5a3315923a03e243a366a07c0e8ce4a0d4af62
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-13 01:15:02 +0530

    added product api with services, controllers

---
+++ DOTNET/ProductsAPI/ProductsSolution/ProductsAPI/DTOs/GetProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsAPI.DTOs
+{
+    public class GetProductDto
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ DOTNET/ProductsAPI/ProductsSolution/ProductsAPI/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using ProductsAPI.DTOs;
+
+namespace ProductsAPI.Services
+{
+    public interface IProductService
+    {
+        List<GetProductDto> GetAll();
+
+        GetProductDto GetById(int id);
+
+        GetProductDto Create(CreateProductDto dto);
+
+        bool Update(int id, UpdateProductDto dto);
+
+        bool Delete(int id);
+    }
+}
+
