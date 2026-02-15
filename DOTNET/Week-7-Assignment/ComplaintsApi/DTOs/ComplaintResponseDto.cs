e7da492a4c783b2faab606234cee5a7ab1b2c9f5
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-15 23:18:06 +0530

    added week 7 assignment

---
+++ DOTNET/Week-7-Assignment/ComplaintsApi/DTOs/ComplaintResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ComplaintsApi.DTOs
+{
+    public class ComplaintResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string CustomerName { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
