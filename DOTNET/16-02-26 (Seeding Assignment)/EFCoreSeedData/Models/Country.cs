7611cc3279b40dc3f83c60f31e925a2575427b89
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-16 21:34:36 +0530

    Added assignment on seeding in ef core

---
+++ DOTNET/16-02-26 (Seeding Assignment)/EFCoreSeedData/Models/Country.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreSeedData.Models
+{
+    [Table("Countries")]
+    public class Country
+    {
+
+        [Key]
+        public int CountryId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string CountryName { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string CountryCode { get; set; }
+        // Navigation Property
+        public List<State> States { get; set; }
+    }
+}
+
---
+++ DOTNET/16-02-26 (Seeding Assignment)/EFCoreSeedData/Models/EFCoreDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EFCoreSeedData.Models
+{
+    public class EFCoreDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configuring the database connection
+            optionsBuilder.UseSqlServer(
+@"Server=(localdb)\MSSQLLocalDB;
+Database=EFCoreDB;
+Trusted_Connection=True;
+TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasData(
+                 new Country { CountryId = 1, CountryName = "India", CountryCode = "IND" },
+                 new Country { CountryId = 2, CountryName = "Australia", CountryCode = "AUS" }
+            );
+            modelBuilder.Entity<State>().HasData(
+                new State { StateId = 1, StateName = "Odisha", CountryId = 1 },
+                new State { StateId = 2, StateName = "Delhi", CountryId = 1 },
+                new State { StateId = 3, StateName = "New South Wales", CountryId = 2 }
+            );
+
+            modelBuilder.Entity<City>().HasData(
+                new City { CityId = 1, CityName = "Bhubaneswar", StateId = 1 },
+                new City { CityId = 2, CityName = "Cuttack", StateId = 1 },
+                new City { CityId = 3, CityName = "New Delhi", StateId = 2 },
+                new City { CityId = 4, CityName = "Sydney", StateId = 3 }
+            );
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+    }
+}
