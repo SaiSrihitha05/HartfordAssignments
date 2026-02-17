8c36c5f83f1582de2852becc103cabba54e0cbc7
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-17 09:41:17 +0530

    Add files via upload

---
+++ DOTNET/Week-7-Assignment/HospitalSolution(Without DB)/HospitalApi/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿namespace HospitalApi.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; } = string.Empty;
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; } = "Scheduled";
+
+        // Foreign Key
+        public int DoctorId { get; set; }
+
+        // Navigation
+        public Doctor? Doctor { get; set; }
+    }
+}
