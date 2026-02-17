cd42c821afae1b868734ffdf8d83222ac7fa23a3
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-17 21:53:27 +0530

    Add files via upload

---
+++ DOTNET/Day35/JWTSolutionDemo/JWTDemo/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        [Authorize]
+
+        public IActionResult Get()
+
+        {
+
+            return Ok(new { value = "This is a protected view" });
+
+        }
+    }
+}
