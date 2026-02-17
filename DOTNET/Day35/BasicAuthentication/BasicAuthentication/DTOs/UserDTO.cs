fd349fd32832de56ef795f4356eb9579cb780e7d
SaiSrihitha05 <153909044+SaiSrihitha05@users.noreply.github.com>
2026-02-17 21:48:57 +0530

    Add files via upload

---
+++ DOTNET/Day35/BasicAuthentication/BasicAuthentication/DTOs/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicAuthentication.DTOs
+{
+    public class UserDTO
+    {
+        //userDto(firstname,lastname,email,password))
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}
---
+++ DOTNET/Day35/BasicAuthentication/BasicAuthentication/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace BasicAuthentication.Models
+{
+    public class User
+    {
+        //(userid,firstname,lastname,email,
+        //password,bool isActive=true,DateTime date= Datetime.now())
+        public int userId { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public bool isActive { get; set; } = true;
+        public DateTime date { get; set; } = DateTime.Now;
+
+    }
+}
